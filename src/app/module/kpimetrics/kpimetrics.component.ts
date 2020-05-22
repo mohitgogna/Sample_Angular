21c2417aaaf0aebb99d8edbf908b9e4eb4fc2d06
320062495 <320062495@YY168039>
2020-05-22 22:59:41 +0530

    add kpi metric

---
+++ src/app/module/kpimetrics/kpimetrics.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kpimetrics',
+  templateUrl: './kpimetrics.component.html',
+  styleUrls: ['./kpimetrics.component.css']
+})
+export class KPIMetricsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
