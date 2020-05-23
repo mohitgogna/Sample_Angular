ff74c8bdf9cd1cf303abbf016c14522676b54839
320062495 <320062495@YY168039>
2020-05-23 21:05:47 +0530

    Add model and dynamic board generation

---
+++ src/app/model/Build.ts
@@ -0,0 +1,12 @@
+export interface Build {
+    Id:string;
+    Name:string;
+    Status:string;
+    Info:string;
+    Version:string;
+    Time:string;
+    Link1:string;
+    Link2:string;
+    Link3:string;
+    Progress:string;
+}
--- src/app/module/digital-board/digital-board.component.ts
+++ src/app/module/digital-board/digital-board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Build } from '../../model/Build';
 
 @Component({
   selector: 'app-digital-board',
@@ -7,29 +8,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DigitalBoardComponent implements OnInit {
 
-  data: any = {
-    "IsBuild": "true",
-  };
+  builds: Build[]=[];
 
-  //dataArray:any=["1","2","3","4"];
-  dataArray: any = [
-    {
-      "Row": "Row 1",
-      "column": ["1", "2", "3", "4"]
-    },
-    {
-      "Row": "Row 2",
-      "column": ["11", "22", "33", "44"]
-    },
-    {
-      "Row": "Row 3",
-      "column": ["111", "222", "333", "444"]
+  dataGenerator(): void {
+    // buildInfo: Build;
+
+    for (let i = 0; i < 10; i++) {
+
+      // this.buildInfo = {
+      //   Id: i,
+      //   Name: "Pipeline " + i,
+      //   Status: "Passed",
+      //   Info: "This is build information of build " + i,
+      //   Version: "v1.1." + i,
+      //   Time: i + "m 24s",
+      //   Link1: "#",
+      //   Link2: "#" + i,
+      //   Link3: "#",
+      //   Progress: "10" + i
+      // };
+
+      this.builds.push({
+        Id: i.toString(),
+        Name: "Pipeline " + i,
+        Status: "Passed",
+        Info: "This is build information of build " + i,
+        Version: "v1.1." + i,
+        Time: i + "m 24s",
+        Link1: "#",
+        Link2: "#" + i,
+        Link3: "#",
+        Progress: "10" + i
+      });
     }
-  ]
 
-  constructor() { }
+    //return this.builds;
+  }
+
+  
+  // buildInfo_1: Build = {
+  //   Id: "2",
+  //   Name: "Search api contract",
+  //   Status: "Passed",
+  //   Info: "This is Build 1 information.",
+  //   Version: "v12.2.4.5",
+  //   Time: "5m 24s 2837ms",
+  //   Link1: "#",
+  //   Link2: "bar",
+  //   Link3: "#",
+  //   Progress: "100"
+  // }
+
+
+  constructor() { 
+    console.log("Called");
+    this.dataGenerator();
+  }
 
   ngOnInit(): void {
+    //this.dataGenerator();
   }
 
   myFunction() {
