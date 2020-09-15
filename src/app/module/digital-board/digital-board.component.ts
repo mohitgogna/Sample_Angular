1009e957bd9823674d342938323bdd5cb96e4560
320062495 <320062495@YY168039>
2020-09-15 23:20:05 +0530

    index check

--- src/app/module/digital-board/digital-board.component.ts
+++ src/app/module/digital-board/digital-board.component.ts
@@ -64,4 +64,16 @@ export class DigitalBoardComponent implements OnInit {
       x.className = "topnav";
     }
   }
+
+  myFunction_test(item) {
+    console.log("111111");
+    console.log(item);
+    console.log("222222");
+    // var x = document.getElementById("myTopnav");
+    // if (x.className === "topnav") {
+    //   x.className += " responsive";
+    // } else {
+    //   x.className = "topnav";
+    // }
+  }
 }
